cd7d3f802ad405d381a92aa729ae16b8aa07108e
imtiazimran <imtiazimran@outlook.com>
2024-04-07 02:42:26 +0600

    Book Ticket updated

--- src/bookSeat/AllCoach.tsx
+++ src/bookSeat/AllCoach.tsx
@@ -1,21 +1,42 @@
 import { useState, useEffect } from "react";
 import moment from "moment";
 import { Button, Card, Typography } from "keep-react";
-import { useFetchDataQuery } from "../redux/api/apiSlice";
+import {
+  useDeleteCoachMutation,
+  useFetchDataQuery,
+} from "../redux/api/apiSlice";
 import { TCoach } from "../utils/types/types";
 import Container from "../utils/Container";
 import Loading from "../utils/Loading";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const AllCoach = () => {
   const { data, isLoading } = useFetchDataQuery() as unknown as {
-    data: { success: boolean; message: string; coaches: TCoach[] };
+    data: { success: boolean; message: string; coach: TCoach[] };
     isLoading: boolean;
   };
+  const [deleteCoach, { isLoading: deleteLoading }] = useDeleteCoachMutation();
+  const coaches = data?.coach;
+  const loading = isLoading || deleteLoading;
 
-  const coaches = data?.coaches;
-
-  console.log(coaches);
+  const handleDeleteCoach = (id: string) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      deleteCoach(id);
+      if (result.isConfirmed) {
+        Swal.fire("Deleted!", "Your file has been deleted.", "success");
+      }
+    });
+    // window.location.reload()
+  };
 
   const [timeRemaining, setTimeRemaining] = useState<string[]>([]);
 
@@ -46,18 +67,19 @@ const AllCoach = () => {
   return (
     <Container>
       <Typography variant="heading-3">All Coach</Typography>
-      {isLoading ? (
+      {loading ? (
         <Loading />
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 ">
           {data &&
-            "coaches" in data &&
+            "coach" in data &&
             coaches.map((coach: TCoach, index: number) => {
               const isExpired = moment(coach.departure).isBefore(moment());
               return (
                 <Card
                   key={coach._id}
                   className={isExpired ? "hidden" : ""}
+                  onDoubleClick={() => handleDeleteCoach(coach._id)}
                 >
                   <Card.Header className="!p-0">
                     <img src={coach.image} alt="image" className="w-full" />
--- src/bookSeat/Booking.tsx
+++ src/bookSeat/Booking.tsx
@@ -1,10 +1,7 @@
 import { Button } from "keep-react";
-import {  useState } from "react";
-import {
-  // useGetCaochQuery,
-  useUpdateSeatMutation,
-} from "../redux/api/apiSlice";
-import { useLoaderData, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useGetCaochQuery, useUpdateSeatMutation } from "../redux/api/apiSlice";
+import { useParams } from "react-router-dom";
 import Container from "../utils/Container";
 import { TCoach } from "../utils/types/types";
 import Swal from "sweetalert2";
@@ -15,58 +12,51 @@ interface Params extends Record<string, string | undefined> {
 
 export const Booking = () => {
   const { id } = useParams<Params>();
-  const data  = useLoaderData() as TCoach
-  // const { coach } = data
-  const { seats, bookedSeats } = data?.coach as TCoach;
+  const [loading, setLoading] = useState(true);
+  const [coachData, setCoachData] = useState<TCoach | null>(null);
+  const { data, isLoading } = useGetCaochQuery(id);
 
-  const [updateSeatMutation, { isLoading: mutaionLoad }] =
-    useUpdateSeatMutation();
+  useEffect(() => {
+    if (data) {
+      setCoachData(data.coach);
+      setLoading(false);
+    }
+  }, [data]);
 
-  const Loading = mutaionLoad;
-
+  const [updateSeatMutation, { isLoading: mutationLoading }] = useUpdateSeatMutation();
   const initializeSeatStatus = (totalSeats: number): string[] => {
     return Array.from({ length: totalSeats }, () => "available");
   };
 
-  const [seatStatus, setSeatStatus] = useState<string[]>(
-    initializeSeatStatus(seats * 4) // Adjust if needed based on the layout
-  );
+  const [seatStatus, setSeatStatus] = useState<string[]>(initializeSeatStatus(coachData ? coachData.seats * 4 : 0));
 
-  const [selectedSeats, setSelectedSeats] = useState<Array<string>>([]);
+  const [selectedSeats, setSelectedSeats] = useState<string[]>([]);
 
-  // Function to handle seat selection
   const handleSeatSelection = (seatName: string) => {
     const seatIndex = selectedSeats.indexOf(seatName);
     const newSelectedSeats = [...selectedSeats];
     const newSeatStatus = [...seatStatus];
 
     if (seatIndex === -1) {
-      // Seat is not selected, so add it to selectedSeats array
       newSelectedSeats.push(seatName);
       newSeatStatus[getSeatIndex(seatName)] = "selected";
     } else {
-      // Seat is already selected, so remove it from selectedSeats array
       newSelectedSeats.splice(seatIndex, 1);
       newSeatStatus[getSeatIndex(seatName)] = "available";
     }
 
-    // Update state
     setSelectedSeats(newSelectedSeats);
     setSeatStatus(newSeatStatus);
   };
-  // Function to handle booking
-  // Function to handle booking
+
   const handleBooking = async () => {
     try {
-      // Call the mutate function from the mutation hook
       const response = await updateSeatMutation({
         id,
-        bookedSeats: [...selectedSeats, ...bookedSeats],
+        bookedSeats: [...selectedSeats, ...(coachData ? coachData.bookedSeats : [])],
       });
 
-      // Check if the mutation was successful
       if ("error" in response) {
-        // Handle error
         console.error("Error updating seats:", response.error);
         Swal.fire({
           icon: "error",
@@ -74,17 +64,14 @@ export const Booking = () => {
           text: "An error occurred while updating seats",
         });
       } else {
-        // Mutation successful
-        setSelectedSeats([]); // Clear selected seats
+        setSelectedSeats([]);
         Swal.fire({
           icon: "success",
           title: "Success",
           text: "Seats updated successfully",
         });
-        window.location.reload();
       }
     } catch (error) {
-      // Handle any other errors
       console.error("An error occurred while updating seats:", error);
       Swal.fire({
         icon: "error",
@@ -94,20 +81,16 @@ export const Booking = () => {
     }
   };
 
-  // Function to handle booking
-
-  // Function to get the index of a seat based on its name (e.g., "A1", "B2")
   const getSeatIndex = (seatName: string): number => {
-    const row = seatName.charCodeAt(0) - 65; // Convert row letter to index (A = 0, B = 1, ...)
-    const col = parseInt(seatName.slice(1)) - 1; // Convert column number to index (1-based to 0-based)
-    return row * 4 + col; // Assuming 4 columns per row
+    const row = seatName.charCodeAt(0) - 65;
+    const col = parseInt(seatName.slice(1)) - 1;
+    return row * 4 + col;
   };
 
-  if (Loading || !seats || !id) {
+  if (loading || mutationLoading || isLoading) {
     return <div>Loading...</div>;
   }
 
-  // Inside your component
   return (
     <Container>
       <Button className="w-full my-7" type="button" onClick={handleBooking}>
@@ -115,14 +98,14 @@ export const Booking = () => {
       </Button>
       <div className="flex justify-center">
         <div className="grid grid-cols-4 gap-4 pb-5">
-          {Array.from({ length: seats }, (_, index) => {
+          {Array.from({ length: coachData ? coachData.seats : 0 }, (_, index) => {
             const seatName = getSeatName(index);
             const status = seatStatus[index];
             return (
               <div
                 key={index}
                 className={`w-10 h-10 rounded p-4 ${
-                  bookedSeats.includes(seatName)
+                  coachData && coachData.bookedSeats.includes(seatName)
                     ? "bg-red-500"
                     : status === "selected"
                     ? "bg-yellow-500"
@@ -140,41 +123,8 @@ export const Booking = () => {
   );
 };
 
-// export const Pill = ({
-//   status,
-//   seatName,
-//   booked,
-//   onClick,
-// }: {
-//   status: string;
-//   seatName: string;
-//   booked: string[];
-//   onClick: () => void;
-// }) => {
-//   let bgColor = "";
-//   if (booked.includes(seatName)) {
-//     bgColor = "bg-red-500";
-//   } else if (status === "selected") {
-//     bgColor = "bg-yellow-500";
-//   } else {
-//     bgColor = "bg-green-500";
-//   }
-
-//   return (
-//     <div>
-//       <div
-//         className={`w-10 h-10 rounded p-4 ${bgColor} text-center`}
-//         onClick={onClick}
-//       >
-//         {seatName}
-//       </div>
-//     </div>
-//   );
-// };
-
-// Function to get the name of a seat based on its index
 const getSeatName = (index: number): string => {
-  const row = String.fromCharCode(65 + Math.floor(index / 4)); // Convert row index to letter (A, B, C, ...)
-  const col = (index % 4) + 1; // Calculate column number (1, 2, 3, 4)
+  const row = String.fromCharCode(65 + Math.floor(index / 4));
+  const col = (index % 4) + 1;
   return `${row}${col}`;
 };
