aefcc077d8ea25b432db1c47815adde1f131287c
imtiazimran <imtiazimran@outlook.com>
2024-04-09 16:28:06 +0600

    update in

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { TCoach } from "../../utils/types/types";
 
-// const baseUrl = "http://localhost:3000/api/v1/coach"
-export const baseUrl = "https://book-ticket-backend.vercel.app/api/v1/coach";
+export const baseUrl = "http://localhost:3000/api/v1/coach"
+// export const baseUrl = "https://book-ticket-backend.vercel.app/api/v1/coach";
 
 
 export const apiSlice = createApi({
